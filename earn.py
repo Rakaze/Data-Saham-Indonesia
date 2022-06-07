c2a33fa27de82a129a38606a88de087b1c4dad58
Raka Luthfi <rakageeee@gmail.com>
2022-06-07 15:23:46 +0700

    remove earn.py

--- earn.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import pandas as pd
-
-kodeEmiten = 'BBCA.JK'
-tickerData1 = yf.Ticker(kodeEmiten)                                        
-tickerDf1 = tickerData1.history() 
-
-dataEarn = pd.DataFrame(tickerData1.earnings.T)
-dataEarn.plot(kind='bar')
